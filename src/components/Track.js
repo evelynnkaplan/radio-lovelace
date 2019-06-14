2ee42c61a4ba7532480f33445dc53581e9716e05
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-06-14 14:38:53 -0700

    refactored Track component to classical and set state, wrote method to check/uncheck and favorite/unfavorite a track

--- src/components/Track.js
+++ src/components/Track.js
@@ -5,18 +5,40 @@ import "./styles/Track.css";
 
 // Here we use destructuring to extract the props into separate variables
 // See https://wesbos.com/destructuring-objects/
-const Track = ({title, artist, playtime, albumart, favorite}) => {
+class Track extends React.Component { 
+  
+  constructor (props) {
+    super();
+    this.state = {
+      title: props.title,
+      artist: props.title,
+      playtime: props.playtime,
+      albumart: props.albumart,
+      favorite: undefined,
+      checked: false, // Setting this value is "controlling" it through React: 
+        // https://reactjs.org/docs/forms.html#controlled-components
+    };
+
+  };
+
+  markFavorite = () => {
+    this.setState({ favorite: !this.state.favorite });
+    this.setState({ checked: !this.state.checked });
+  };
+  
+  render () {
   return (
     <li className="track">
-      <img className="track--albumart" alt={`album art for ${title}`} src={albumart} />
-      <h3 className="track--title">{title}</h3>
+      <img className="track--albumart" alt={`album art for ${this.state.title}`} src={this.state.albumart} />
+      <h3 className="track--title">{this.state.title}</h3>
       <input
         type="checkbox"
         className="track--favorite"
-        checked={!favorite}
+        checked={this.state.checked}
+        onChange={this.markFavorite}
       />
-      <p className="track--artist">{artist}</p>
-      <p className="track--playtime">{playtime}</p>
+      <p className="track--artist">{this.state.artist}</p>
+      <p className="track--playtime">{this.state.playtime}</p>
       <button
         className="track--control track--to-top"
         >
@@ -31,12 +53,6 @@ const Track = ({title, artist, playtime, albumart, favorite}) => {
   );
 };
 
-Track.propTypes = {
-  title: PropTypes.string,
-  artist: PropTypes.string,
-  playtime: PropTypes.string,
-  albumart: PropTypes.string,
-  favorite: PropTypes.bool,
 }
 
 export default Track;
